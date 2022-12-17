e0cb9dd4a82b97af06ee1312b32a9431d032882b
abduco <110682161+nyxan@users.noreply.github.com>
2022-12-18 02:14:12 +0530

    added all solar system planets and other minor changes

--- sandbox.py
+++ sandbox.py
@@ -9,15 +9,16 @@ import math
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((1620,960))
+screensize = 1280,720
+screen = pygame.display.set_mode(screensize)
 pygame.display.set_caption('simulation')
 clock = pygame.time.Clock()
 
 # variables for revolution of the planet
 # x-axis, y-axis, angle(to rotate it)
-x=200
-y=200
-theta=0
+# h,k centre of screen
+x,y,theta = 50,50,0
+h,k = screensize[0]/2, screensize[1]/2
 
 # images used
 # convert() converts the image to more optimized form for pygame 
@@ -25,42 +26,71 @@ theta=0
 # convert_alpha() keeps it bg-less
 bg_surf = pygame.image.load('graphics/starbg1.jpg').convert()
 star_surf = pygame.image.load('graphics/sun.png').convert_alpha()
-planet_surf = pygame.image.load('graphics/planet3.png').convert_alpha()
+star_rect = star_surf.get_rect(center = (h,k))
 
-planet_rect = planet_surf.get_rect(topleft = (x,y))
+planets = {}            # all the planets in the folder
+revolving = {}          # the planets which are revolving
+try:
+    n=1
+    while True:
+        planet_surf = pygame.image.load(f'graphics/planet{n}.png').convert_alpha()
+        planet_rect = planet_surf.get_rect(center = (x,y))
+        planets[f'pla{n}'] = [planet_surf, planet_rect]
+        revolving[f'pla{n}'] = 0
+        n += 1
+except: None
 
-while True:
-    
-    # to close the window when user clicks on close
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        
-    # display the image 
-    # blit (block image transfer)
-    # blit(imagehandle,position)
-    screen.blit(bg_surf,(0,0))
-    screen.blit(star_surf,(760,440))
-    
+def revolve(n):   
     # the math:
-    #     +800 and +490 are the cenre of the circle (let it be origin)
+    #     (h-15) and (k-15) are the cenre of the circle (let it be origin)
+    #     x and y decide the radius
     #     theta starts from 0 i.e. at:
     #         0  : cos is max and sin is 0
     #         90 : cos is 0 and sin is max
     #         180: cos is min and sin is 0
     #         270: cos is 0 and sin is min
     #         after 360 : repeat
-    #     value at x-axis is determined by cos and value at y-axis is determined by sin
-    screen.blit(planet_surf,planet_rect)
-    planet_rect.x = x*math.cos(theta)+800
-    planet_rect.y = y*math.sin(theta)+490
+    #     value at x-axis is determined by cos and value at y-axis is determined by sin 
+    global x,y,theta
+    plaobj = planets[f'pla{n}']
+    screen.blit(plaobj[0],plaobj[1])
+    plaobj[1].x = x*math.cos(theta)+(h-15)
+    plaobj[1].y = y*math.sin(theta)+(k-15)
     theta += 0.01
 
+
+running = ' '
+while running:
+    # to close the window when user clicks on close
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = 0
+        
+    # to display the image 
+    # blit - block image transfer
+    # blit(imagehandle,position)
+    screen.blit(bg_surf,(0,0))
+    screen.blit(star_surf,star_rect)
+    
+    # to know if the user has pressed any mouse button and to get the position of the mouse
+    mouse_press = pygame.mouse.get_pressed()
+    mouse_pos = pygame.mouse.get_pos()
+
+    # mouse_press (left button,middle button,right button)
+    if mouse_press == (1,0,0) or mouse_press == (1,1,0) or mouse_press == (1,0,1):
+        x,y = mouse_pos
+        x,y = x-h,y-k
+        revolving[f'pla{3}'] = 1
+        
+    for i in range(1,n):
+        if revolving[f'pla{i}'] == 1:
+            revolve(i)
+    
     pygame.display.update()
 
-    mouse_press = pygame.mouse.get_pressed()
-    # to slow down time by getting less fps when mouse1 is clicked
-    if mouse_press == (1,0,0): 
-            clock.tick(24)
-    else: clock.tick(90)
+    # fps
+    clock.tick(90)
+
+if running == 0:
+    pygame.quit()
+    exit()
